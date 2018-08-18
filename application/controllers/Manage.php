d2b97ea9e0fd0e5a13fc3c4727b7fab5ffedf26c
Mister Zulgo <ijoi7007@gmail.com>
2018-08-18 08:55:32 +0800

    add manage.php

---
+++ application/controllers/Manage.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Manage extends CI_Controller
+{
+
+    function __construct()
+    {
+        // this is your constructor
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->database();
+        $this->load->model('wasap_model');
+        $this->load->library('grocery_CRUD');
+    }
+
+    public function index()
+    {
+        $this->grocery_crud->set_table('tbl_wasap');
+
+        $output = $this->grocery_crud->render();
+
+        $this->_e_output((array)$output);
+    }
+
+    public function _e_output($output = null)
+    {
+        //$this->load->view('example', $output);
+        $this->load->view('link-crud', (array)$output);
+    }
+}
+
+
---
+++ application/logs/log-2018-08-18.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 18-08-2018 07:46:58 --> 404 Page Not Found: /index
+ERROR - 18-08-2018 07:47:08 --> 404 Page Not Found: Managelink/index
+ERROR - 18-08-2018 07:47:14 --> 404 Page Not Found: Managelink/index
+ERROR - 18-08-2018 07:47:17 --> 404 Page Not Found: Managelink/index
